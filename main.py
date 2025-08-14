68d594427a01d2e5939258236a5d0c5150c75db8
RuslanSho <ruslanshostak21@gmail.com>
2025-08-14 20:02:39 +0300

    UPD2025.08.14

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import sqlite3
 from common import user_folder
 import os
+import numpy as np
 
 start_date = pd.to_datetime('today').date() - pd.Timedelta(days=181)
 start_date = "2025-05-30" if start_date < pd.to_datetime("2025-05-30").date() else start_date
@@ -12,6 +13,16 @@ end_date = pd.to_datetime('today').date() - pd.Timedelta(days=1)
 inv = database_tools.read_database('fba_inventory', start_date, pd.to_datetime('today').date())
 sales = database_tools.read_database('sales', start_date, end_date)
 
+dictionary_path = r'G:\Shared drives\30 Sales\30.1 MELLANNI\30.11 AMAZON\30.111 US\Sales\Dictionary.xlsx'
+dictionary = pd.read_excel(dictionary_path, usecols=['ASIN','Collection','Size','Color'])
+dictionary = dictionary.rename(columns={'ASIN':'asin', 'Collection':'collection', 'Size':'size', 'Color':'color'})
+dictionary = dictionary.drop_duplicates('asin')
+
+sets_in_a_box_path = r'G:\My Drive\Все файлы\AMAZON\ASIN dimensions_2.xlsx'
+sets_in_a_box = pd.read_excel(sets_in_a_box_path, usecols=['asin','sets_in_a_box'])
+
+days_of_sale = 49
+
 def calculate_inventory_isr(inventory):
     filter = inventory['sku'].str.endswith('-CA') # Create a filter for Canadian SKUs ONLY
 
@@ -20,7 +31,7 @@ def calculate_inventory_isr(inventory):
     total_days = inventory_grouped['snapshot-date'].nunique()
 
     inventory_grouped['in_stock?'] = inventory_grouped['inventory_supply_at_fba'] > 0
-    asin_isr = inventory_grouped.groupby('asin')[['in_stock?']].agg('mean').reset_index()
+    asin_isr = (inventory_grouped.groupby('asin')[['in_stock?']].agg('mean').reset_index()).round(2)
     return asin_isr, total_days
 
 
@@ -59,8 +70,8 @@ def last_2_weeks_sales(df_sales:pd.DataFrame, df_inventory:pd.DataFrame):
     latest_sales = pd.merge(latest_sales, latest_isr, on='(child)_asin', how='outer')
     # latest_sales = latest_sales.rename(columns={'(child)_asin': 'asin'})
     latest_sales['average_2_weeks_units'] = latest_sales['units_ordered'] / 14
-    latest_sales['average_2_weeks_sessions'] = latest_sales['sessions_-_total'] / 14
-    latest_sales['average_2_weeks_units_corrected'] = latest_sales['average_2_weeks_units'] / latest_sales['in_stock?']
+    latest_sales['average_2_weeks_sessions'] = (latest_sales['sessions_-_total'] / 14).round(3)
+    latest_sales['average_2_weeks_units_corrected'] = (latest_sales['average_2_weeks_units'] / latest_sales['in_stock?']).round(3)
     return latest_sales[['(child)_asin','average_2_weeks_units_corrected', 'average_2_weeks_sessions']]
 
 
@@ -74,8 +85,8 @@ def get_asin_sales(sales, total_day):
 
 
     total_sales = sales_daily.groupby('(child)_asin')[['units_ordered','sessions_-_total']].agg('sum').reset_index()
-    total_sales['average_daily_units'] = total_sales['units_ordered'] / total_day
-    total_sales['average_daily_sessions'] = total_sales['sessions_-_total'] / total_day
+    total_sales['average_daily_units'] = (total_sales['units_ordered'] / total_day).round(3)
+    total_sales['average_daily_sessions'] = (total_sales['sessions_-_total'] / total_day).round(3)
     total_sales = pd.merge(total_sales, latest_sales, on='(child)_asin', how='outer')
     total_sales = total_sales.rename(columns={'(child)_asin': 'asin'})
     return total_sales
@@ -85,18 +96,30 @@ def main():
     asin_isr, total_day = calculate_inventory_isr(inv[['snapshot-date', 'sku', 'asin', 'inventory_supply_at_fba']].copy())
     total_sales = get_asin_sales(sales, total_day)
     result = pd.merge(asin_isr, total_sales, on='asin', how='outer')
-    result['average_corrected_long'] = result['average_daily_units'] / result['in_stock?']
+    result['in_stock?'] = result['in_stock?'].fillna(0)
+    result['units_ordered'] = result['units_ordered'].fillna(0)
+    result['sessions_-_total'] = result['sessions_-_total'].fillna(0)
+    result['average_daily_units'] = result['average_daily_units'].fillna(0)
+    result['average_daily_sessions'] = result['average_daily_sessions'].fillna(0)
+    result['average_corrected_long'] = (result['average_daily_units'] / result['in_stock?']).round(3)
     result['average_corrected_long'] = result['average_corrected_long'].fillna(0)
     result['average_2_weeks_units_corrected'] = result['average_2_weeks_units_corrected'].fillna(0)
     # result['average_corrected'] = result[['average_corrected_long','average_2_weeks_units_corrected']].mean(axis=1) #use `axis = ` to specify the direction of the mean calculation (rows)
-    result['average_corrected'] = result['average_corrected_long']*0.4 + result['average_2_weeks_units_corrected'] *0.6
+    result['average_corrected'] = (result['average_corrected_long']*0.4 + result['average_2_weeks_units_corrected'] *0.6).round(3)
     inv_copy = inv.copy()
     inv_copy['snapshot-date'] = pd.to_datetime(inv_copy['snapshot-date'])
     latest_date = inv_copy['snapshot-date'].max()
     latest_inventory = inv_copy[inv_copy['snapshot-date'] == latest_date]
     quantity_map = latest_inventory.groupby('asin')['inventory_supply_at_fba'].sum().reset_index()
     result = pd.merge(result, quantity_map, on='asin', how='outer')
-    result['days_of_sale_remaining'] = result['inventory_supply_at_fba'] /result['average_corrected']
+    result['inventory_supply_at_fba'] = result['inventory_supply_at_fba'].fillna(0)
+    result['days_of_sale_remaining'] = (result['inventory_supply_at_fba'] / result['average_corrected']).round(0)
+    result['days_of_sale_remaining'] = result['days_of_sale_remaining'].fillna(0)
+    result['to_ship_units'] = ((result['average_corrected'] * days_of_sale - result['inventory_supply_at_fba']).round(0)).apply(lambda x: x if x > 0 else 0)
+    result = pd.merge(result, sets_in_a_box, on='asin', how='inner')
+    result['to_ship_boxes'] = (np.ceil(result['to_ship_units'] / result['sets_in_a_box'])).apply(lambda x: x if x > 0 else 0)
+    result['dos_with_to_ship_boxes'] = (((result['sets_in_a_box'] * result['to_ship_boxes'] + result['inventory_supply_at_fba']) / result['average_corrected']).round(0)).apply(lambda x: x if x > 0 else 0)
+    result = pd.merge(result, dictionary, on='asin', how='inner')
     result.to_excel(os.path.join(user_folder, 'inventory_restock.xlsx'), index=False)
     os.startfile(os.path.join(user_folder))
 
